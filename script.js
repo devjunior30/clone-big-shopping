ac27eff93e3c5954e3f0c7bc69f60534d1fcd7c8
devjunior30 <edyjunior299@gmail.com>
2023-06-05 16:29:58 -0300

    ESQUECI DE FAZER O REPOSITORIO KKKK, metade dos elementos na tela e iniciando a estilização deles

---
+++ script.js
@@ -0,0 +1,19 @@
+let time = 2000,
+      currentImageIndex = 0,
+      image = document
+                   .querySelectorAll("slider img")
+        max = images.lenght;
+
+        function start() {
+            setInterval(() => {
+
+                console.log("função rodada")
+
+            }, time) 
+            
+            
+            
+            
+        }
+
+        window.addEventListener("load", start)
